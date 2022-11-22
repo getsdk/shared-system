2d652de4016837ec3609e135ea8348e1a3023ba3
bu0nq <67270989+bu0nq@users.noreply.github.com>
2022-11-22 23:23:30 +0300

    Create AbstractBootstrapProvider.php

---
+++ src/AbstractBootstrapProvider.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace GetSDK\Shared\System;
+
+use Illuminate\Support\ServiceProvider as AbstractLaravelProvider;
+
+/**
+ * AbstractBootstrapProvider
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractBootstrapProvider extends AbstractLaravelProvider
+{
+
+}
